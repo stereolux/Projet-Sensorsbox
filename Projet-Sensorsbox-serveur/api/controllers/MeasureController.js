d891a776e7e1fb330a11b44fd7eaf9f141d3c4c8
Vincent Kammerer <vkammerer@gmail.com>
2014-05-29 17:30:22 +0200

    Projet-Sensorsbox-serveur: fixed EmailService Error messaging and added error sending on Sensor request

--- Projet-Sensorsbox-serveur/api/controllers/MeasureController.js
+++ Projet-Sensorsbox-serveur/api/controllers/MeasureController.js
@@ -10,6 +10,8 @@ module.exports = {
 	create : function(req, res) {
 		if (!RecordService.recordTimeouts[req.body.sensor]) {
 			Sensor.find(req.body.sensor).exec(function(err, sensors) {
+				var subject = 'Measures creation - Failed to find sensor to create timeout';
+				EmailService.sendError(subject, err);
 				RecordService.recordSensor(sensors[0]);
 			});
 		}
